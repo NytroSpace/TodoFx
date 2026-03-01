fc8f6a7a8e42d33312c849264e11cda484e70bd4
Rannikkoluoto Nico Kristian <nico.k.rannikkoluoto@student.jyu.fi>
2026-03-01 23:36:30 +0200

    Tehtava 7.5 after commit

--- src/main/java/fi/jyu/ohj2/nico/todo/Tehtava.java
+++ src/main/java/fi/jyu/ohj2/nico/todo/Tehtava.java
@@ -1,4 +1,20 @@
 package fi.jyu.ohj2.nico.todo;
 
 public class Tehtava {
+    @SuppressWarnings("FieldMayBeFinal")
+    private String teksti;
+    @SuppressWarnings("FieldMayBeFinal")
+    private boolean tehty;
+
+    @SuppressWarnings("unused")
+    public Tehtava() {}
+
+    public Tehtava(String teksti, boolean tehty) {
+        this.teksti = teksti;
+        this.tehty = tehty;
+    }
+
+    public boolean getTehty() { return tehty; }
+
+    public String getTeksti() { return teksti; }
 }
